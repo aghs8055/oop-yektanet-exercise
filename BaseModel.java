c5cb69b656ecc71f4954a27346ef868b0993d2c6
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-05 17:53:45 +0330

    BaseModel.java created with common fields and methods of Ad and Advertiser classes.

---
+++ BaseModel.java
@@ -0,0 +1,29 @@
+public abstract class BaseModel {
+    private int id;
+    private int clicks;
+    private int views;
+
+    public BaseModel(int id) {
+        this.id = id;
+        this.clicks = 0;
+        this.views = 0;
+    }
+
+    public void incClicks() {
+        this.clicks++;
+    }
+
+    public void incViews() {
+        this.views++;
+    }
+
+    public int getClicks() {
+        return this.clicks;
+    }
+
+    public int getViews() {
+        return this.views;
+    }
+
+    public abstract void discribeMe();
+}
