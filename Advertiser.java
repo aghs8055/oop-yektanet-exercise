5eb59a13c88a12e8217032c2cd3b8a577b9d9f9b
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-05 18:37:08 +0330

    Advertiser.java added with specified fields and methods.

---
+++ Advertiser.java
@@ -0,0 +1,37 @@
+public class Advertiser extends BaseModel {
+    private String name;
+    private static int totalClick;
+
+    public Advertiser(int id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void incClicks() {
+        this.clicks++;
+        totalClick++;
+    }
+
+    public static int getTotalClick() {
+        return totalClick
+    }
+
+    public void describeMe() {
+        System.out.println("This class contains fields and methods for system advertisers.");
+    }
+
+    public void help() {
+        System.out.println("id: This unique field keep the id of advertiser in a int variable.\n" +
+                "name: This field is name of advertiser and sort in String.\n" +
+                "clicks: This field save the count of clicks of advertiser.\n" +
+                "viwes: This field keep the count of views of advertiser.")
+    }
+}
