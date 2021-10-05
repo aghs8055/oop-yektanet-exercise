1f636a202e1e9d20b81f4315147dc8923bcfa07f
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-05 18:38:14 +0330

    Ad.java created with specified fields and methods.

---
+++ Ad.java
@@ -0,0 +1,46 @@
+public class Ad extends BaseModel {
+    private String title;
+    private String imgUrl;
+    private String link;
+    private Advertiser advertiser;
+
+    public Ad(int id, String title, String imgUrl, String link, Advertiser advertiser) {
+        super(id);
+        this.title = title;
+        this.imgUrl = imgUrl;
+        this.link = link;
+        this.advertiser = advertiser;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setAdvertiser(Advertiser advertiser) {
+        this.advertiser = advertiser;
+    }
+
+    public void describeMe() {
+        System.out.println("This class contains fields and methods related to advertising.");
+    }
+}
